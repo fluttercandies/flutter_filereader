2d654685e7ccbea0978456a474a918b86bc3ee0f
shingohu <shingohu@163.com>
2019-03-20 10:48:56 +0800

    新增一个是否打开的回调

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,7 +1,7 @@
 import 'dart:io';
-
 import 'package:flutter/material.dart';
 import 'package:flutter/foundation.dart';
+
 import 'package:flutter_filereader_example/file.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:flutter_downloader/flutter_downloader.dart';
