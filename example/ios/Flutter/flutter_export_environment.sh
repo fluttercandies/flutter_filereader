ac95920682d84a00ab0b94a0961f5c64ee3c1141
shingohu <shingohu@163.com>
2019-10-22 09:49:23 +0800

    删除无用代码

---
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/hujie/MEGA/Flutter/tool/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/hujie/MEGA/Flutter/my_flutter_plugins/flutter_filereader/example"
+export "FLUTTER_TARGET=/Users/hujie/MEGA/Flutter/my_flutter_plugins/flutter_filereader/example/lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/hujie/MEGA/Flutter/tool/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_BUILD_NAME=1.3.6"
+export "FLUTTER_BUILD_NUMBER=11"
+export "TRACK_WIDGET_CREATION=true"
