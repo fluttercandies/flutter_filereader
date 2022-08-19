40defef9d3a00e79f708e0cea2e5468c8bc279f9
陈汝冰 <chenrubing@yaodaitong.com.cn>
2022-08-19 11:12:22 +0800

    更新jar

--- example/ios/Flutter/flutter_export_environment.sh
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/hujie/Develop/FlutterSDK/flutter-2.x"
-export "FLUTTER_APPLICATION_PATH=/Users/hujie/Develop/Flutter/flutter_filereader/example"
+export "FLUTTER_ROOT=/Users/bing/fvm/versions/stable"
+export "FLUTTER_APPLICATION_PATH=/Users/bing/workspace/flutter_filereader/example"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
-export "SYMROOT=${SOURCE_ROOT}/../build/ios"
 export "FLUTTER_BUILD_NAME=1.3.6"
 export "FLUTTER_BUILD_NUMBER=11"
 export "DART_OBFUSCATION=false"
 export "TRACK_WIDGET_CREATION=false"
 export "TREE_SHAKE_ICONS=false"
-export "PACKAGE_CONFIG=.packages"
+export "PACKAGE_CONFIG=.dart_tool/package_config.json"
